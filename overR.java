037e0a482f19af706347aa52718b09d3af4feb92
Arnav Thakur <bus.arnavt@gmail.com>
2024-09-18 11:27:41 +0530

    Add files via upload

---
+++ overR.java
@@ -0,0 +1,20 @@
+package Practice.Overriding;
+
+public class overR {
+
+    void add(int a) {
+
+        int c;
+        int b = 10;
+        c=a+b;
+        System.out.println(c);
+    }
+
+    void add(int a, int b) {
+
+        int c;
+        c=a+b;
+        System.out.println(c);
+    }
+}
+
